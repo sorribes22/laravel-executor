bc408310031a808b64cd3df5f46f6b381a110946
Àlex Sorribes <alexsorribes22@gmail.com>
2019-08-26 12:47:06 +0200

    Apply fixes from StyleCI (#2)

--- tests/Unit/ExecutorTest.php
+++ tests/Unit/ExecutorTest.php
@@ -20,14 +20,14 @@ class ExecutorTest extends ExecutorTestCase
     /** @test */
     public function can_fake_result_and_return()
     {
-        Executor::shouldReceive('run')->with('ping google.com', 4)->once()->andReturn("PING google.com (172.217.17.14) 56(84) bytes of data.
-64 bytes from mad07s09-in-f14.1e100.net (172.217.17.14): icmp_seq=1 ttl=51 time=25.9 ms");
+        Executor::shouldReceive('run')->with('ping google.com', 4)->once()->andReturn('PING google.com (172.217.17.14) 56(84) bytes of data.
+64 bytes from mad07s09-in-f14.1e100.net (172.217.17.14): icmp_seq=1 ttl=51 time=25.9 ms');
 
         $result = 4;
         $return = Executor::run('ping google.com', $result);
 
         $this->assertEquals(4, $result);
-        $this->assertEquals("PING google.com (172.217.17.14) 56(84) bytes of data.
-64 bytes from mad07s09-in-f14.1e100.net (172.217.17.14): icmp_seq=1 ttl=51 time=25.9 ms", $return);
+        $this->assertEquals('PING google.com (172.217.17.14) 56(84) bytes of data.
+64 bytes from mad07s09-in-f14.1e100.net (172.217.17.14): icmp_seq=1 ttl=51 time=25.9 ms', $return);
     }
 }
